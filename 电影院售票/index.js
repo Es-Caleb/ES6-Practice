5e7c144a596f98b285048ec95b096a3513a6998b
E_Chan <chenliangbo@yooleap.com>
2021-03-31 16:27:09 +0800

    2021-3-31

--- 电影院售票/index.js
+++ 电影院售票/index.js
@@ -1,33 +1,37 @@
 class Point {
-    yingTing(x,y) {
-        var yyuan = [];
+    cinema(x,y) {
+        let yyuan = [];
         for(let i = 0; i < x; i++){
             let zuowei = new Array(y).fill('O');
-            yyuan.push(zuowei)
+            yyuan.push(zuowei);
         }
         this.yyuan = yyuan;
     }
     // 购票
-    gouPiao(a,b) {
+    buyTicket(a,b) {
         this.yyuan[a-1][b-1] = 'X';
         let shouHou = '';
         for (let j = 0; j < this.yyuan.length; j++) {
-            this.shouHou = shouHou += this.yyuan[j] + '\n';
+            this.buys = shouHou += this.yyuan[j] + '\n';
         }
     }
     // 退票
     refund(a,b) {
         this.yyuan[a-1][b-1] = 'O';
-        let shouHou = '';
         for (let j = 0; j < this.yyuan.length; j++) {
-            this.shouHou = shouHou += this.yyuan[j] + '\n';
+            this.buys += this.yyuan[j] + '\n';
         }
-        this.tuiPiao = new Date().toLocaleString() + ' row ' + a + ' column ' + b + ' refund';
+        let date = new Date();
+        let year = date.getFullYear() + '-';
+        let month = date.getMonth() + 1 + '-';
+        let day = date.getDate() + ' ';
+        let time = date.toLocaleTimeString('chinese', {hour12: false});
+        this.refTicket = year + month + day + time + ' row ' + a + ' column ' + b + ' refund';
     }
 }
 var arr = new Point();
-arr.yingTing(5,10);
-arr.gouPiao(4,6);
+arr.cinema(5,10);
+arr.buyTicket(4,6);
+console.log(arr.buys);
 arr.refund(4,6);
-console.log(arr.shouHou);
-console.log(arr.tuiPiao);
+console.log(arr.refTicket);
